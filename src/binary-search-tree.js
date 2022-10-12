4ff6202f8b2db3f0fdfa6d84d2179d181230ee53
Dmitrii23748 <chunosov.dmitrij@bk.ru>
2022-10-12 21:59:21 +0300

    feat: add binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,121 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.startNode = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.startNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add( data, startNode = this.startNode) {
+    if (startNode == null) {
+      this.startNode = new Node(data);
+      return;
+    }
+    
+    if (data < startNode.data) {
+      if (startNode.left == null) {
+        startNode.left = new Node(data);
+      } else {
+        this.add(data, startNode.left);
+      }
+    }
+    if (data > startNode.data) {
+      if (startNode.right == null) {
+        startNode.right = new Node(data);
+      } else {
+        this.add(data, startNode.right);
+      }
+    }
+    
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.find(data, this.startNode)) {
+      return true;
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  find(data, startNode = this.startNode) {
+    if (startNode.data == data) {
+      return startNode;
+    }
+    if (data < startNode.data) {
+      if (startNode.left == null) {
+        return null;
+      } else {
+        return this.find(data, startNode.left);
+      }
+    } else if (data > startNode.data) {
+      if (startNode.right == null) {
+        return null;
+      } else {
+        return this.find(data, startNode.right);
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    if (this.has(data)) {
+      this.startNode = removeWithin(this.startNode, data);
+
+      function removeWithin(node, data) {
+        if (node.data == data) {
+          if (node.left == null && node.right == null) {
+            return null;
+          } else if (node.left == null) {
+            node = node.right;
+            return node;
+          } else if (node.right == null) {
+            node = node.left;
+            return node;
+          } else {
+            if (node.right.left == null) {
+              node.data = node.right.data;
+              node.right = node.right.right;
+              return node;
+            }
+            let rem = minLeft(node.right);
+            node = removeWithin(node, rem.data);
+            node.data = rem.data;
+
+            function minLeft(node) {
+              while (node.left) {
+                node = node.left;
+              }
+              return node;
+            }
+            return node;
+          }
+        }
+        if (node.data < data) {
+          node.right = removeWithin(node.right, data);
+          return node;
+        } else {
+          node.left = removeWithin(node.left, data);
+          return node;
+        }
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let factValue = this.startNode;
+    while (factValue.left) {
+      factValue = factValue.left;
+    }
+    return factValue.data;
   }
-
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let factValue = this.startNode;
+    while (factValue.right) {
+      factValue = factValue.right;
+    }
+    return factValue.data;
   }
 }
 
