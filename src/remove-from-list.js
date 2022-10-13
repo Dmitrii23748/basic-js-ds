1b3aacf3298455ac88e66e22c1bb25c8761f57b5
Dmitrii23748 <chunosov.dmitrij@bk.ru>
2022-10-13 20:47:09 +0300

    feat: stack and remove

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k) {
+  if (l.value === k) {
+    l = l.next;
+  }
+  let number = l;
+  if (number !== null) {
+    while (number.next) {
+      if (number.next.value === k) {
+        number.next = number.next.next;
+      } else {
+        number = number.next;
+      }
+    }
+  }
+
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
